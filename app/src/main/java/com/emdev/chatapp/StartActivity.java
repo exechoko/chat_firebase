294e2c3d004c033cceaa58fddbf119bc8d9fcf90
Exequiel Marichal <exemarichal@gmail.com>
2021-06-29 13:17:11 -0300

    Registro y login de usuarios

---
+++ app/src/main/java/com/emdev/chatapp/StartActivity.java
@@ -0,0 +1,39 @@
+package com.emdev.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login, register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogin = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intentLogin);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegister = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intentRegister);
+            }
+        });
+
+    }
+}
